2f2e1e42fdac7566fa2187139d797dc12de95e94
Breyanna Vass <breyannavass@gmail.com>
2019-11-25 16:03:57 -0500

    adding fields to each input in the movie entity

--- typescript-graphql-crud/src/entity/Movie.ts
+++ typescript-graphql-crud/src/entity/Movie.ts
@@ -1,13 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { Field, Int } from "type-graphql";
 
 @Entity()
 export class Movie extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   title: string;
 
+  @Field(() => Int)
   @Column("int", { default: 60 })
   minutes: number;
 }
